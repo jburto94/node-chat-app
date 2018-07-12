f04f50733fe020c18b20864c6169395ca90738cf
jburto94 <jburto94@yahoo.com>
2018-07-12 11:13:42 -0400

    Emit newMessage on createMessage

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Hunter',
-    text: 'This is from Hunter'
-  });
 });
 
 socket.on('disconnect', function() {
