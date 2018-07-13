a0b32921fa41179aba62dc46cad55c4094c134ae
jburto94 <jburto94@yahoo.com>
2018-07-12 22:54:23 -0400

    Scroll to bottom if user is close to bottom

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,22 @@
 const socket = io();
 
+function scrollToBottom() {
+  //Selectors
+  const messages = $('#messages');
+  const newMessage = messages.children('li:last-child');
+  
+  //Heights
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('Connected to server');
 });
@@ -18,10 +35,7 @@ socket.on('newMessage', function(message) {
   });
 
   $('#messages').append(html);
-  // const li = $('<li></li>');
-  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-  // $('#messages').append(li);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -34,6 +48,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 const messageTextBox = $('[name=message]')
