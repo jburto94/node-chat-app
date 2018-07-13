6e33e81ca4e111254270811de6d523614381f098
jburto94 <jburto94@yahoo.com>
2018-07-12 21:12:09 -0400

    Added geolocation support via geolocation api

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -16,6 +16,17 @@ socket.on('newMessage', function(message) {
   $('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message) {
+  const li = $('<li></li>');
+  const a = $('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+
+  li.append(a);
+  $('#messages').append(li);
+});
+
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
 
@@ -26,3 +37,22 @@ $('#message-form').on('submit', function(e) {
     
   });
 });
+
+const locationButton = $('#send-location');
+
+locationButton.on('click', function() {
+  if (!navigator.geolocation) {
+    return alert('Your browser does not support geolocation');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position);
+
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function() {
+    alert('Unable to fetch location');
+  });
+})
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message Object', () => {
@@ -11,4 +11,17 @@ describe('generateMessage', () => {
     expect(typeof message.createdAt).toBe('number');
     expect(message).toMatchObject({from, text});
   });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const from = 'test';
+    const latitude = 37;
+    const longitude = -95;
+    const url = `https://www.google.com/maps?q=37,-95`;
+    const message = generateLocationMessage(from, latitude, longitude);
+
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toMatchObject({from, url});
+  })
 });
